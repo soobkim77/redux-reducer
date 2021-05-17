9295b99fe369661d670e577213876cbfcff16058
Soo-Beom <soobeomkim77@gmail.com>
2021-05-17 11:01:19 -0700

    commit tree 360a0037dfb8e2e9ec754e69c7f359ffbf4c68c2

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        case 'ADD_FRIEND': 
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            let remove = state.friends.findIndex(friend => friend.id === action.id)
+            return {friends: [...state.friends.slice(0, remove), ...state.friends.slice(remove + 1)]}
+        default: 
+            return state;
+    }
 }
